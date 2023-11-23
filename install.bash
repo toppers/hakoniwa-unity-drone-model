448630b7d2a3a6c294a0446fed02b04a70217712
Takashi Mori <kanetugu2015@gmail.com>
2023-11-23 19:39:12 +0900

    bug fixed

--- install.bash
+++ install.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cp drone/hakoniwa_path.json plugin/plugin-srcs/
-cp -rp drone/ros_types/* plugin/plugin-srcs/ros_types/
 cp -rp drone/Assets/* plugin/plugin-srcs/Assets/
 cp -rp drone/hakoniwa-base/* hakoniwa-base/
 
 cd plugin
 
 bash install.bash
+
+cd ..
+cp -rp drone/ros_types/* plugin/plugin-srcs/ros_types/
