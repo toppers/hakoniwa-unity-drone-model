e1a9816fe9beb90376600d32b5d6da5c9512d7f2
Takashi Mori <kanetugu2015@gmail.com>
2024-06-04 14:10:43 +0900

    ar rotor

---
+++ drone/Assets/Scripts/PluggableAsset/Assets/Robot/Drone/Px4/DroneAvatorRotors.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hakoniwa.PluggableAsset.Assets.Robot.Parts;
+
+public class DroneAvatorRotors : MonoBehaviour
+{
+    private DroneRotor[] rotors;
+    public float initial_pos = -0.61f;
+    // Use this for initialization
+    void Start()
+    {
+        rotors = this.GetComponentsInChildren<DroneRotor>();
+        foreach (var rotor in rotors)
+        {
+            rotor.Initialize(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y > initial_pos)
+        {
+            foreach (var rotor in rotors)
+            {
+                rotor.AddForce(0.5f);
+            }
+        }
+        else
+        {
+            foreach (var rotor in rotors)
+            {
+                rotor.AddForce(0.0f);
+            }
+        }
+    }
+}
