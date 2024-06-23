6570ece366a1b1baefa90aae55a1ed30af9f1220
Takashi Mori <kanetugu2015@gmail.com>
2024-06-23 10:40:28 +0900

    updated for latest

--- update_source.bash
+++ update_source.bash
@@ -31,6 +31,8 @@ cp -rp plugin/plugin-srcs/Assets/Prefab/Robots/AR/Drone/* \
     drone/Assets/Prefab/Robots/AR/Drone/
 cp -rp  plugin/plugin-srcs/Assets/Prefab/Robots/Twin/MiniRobotAvator* \
     drone/Assets/Prefab/Robots/Twin/
+cp -rp plugin/plugin-srcs/Assets/Prefab/Robots/TB3RoboAvatar* \
+    drone/Assets/Prefab/Robots/
 
 #Model
 cp -rp plugin/plugin-srcs/Assets/Model/Hakoniwa/Robots/Drone/* \
