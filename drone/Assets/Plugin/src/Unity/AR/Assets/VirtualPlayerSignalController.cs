cbd4041465cb4e165371e89fc723d70b96741018
Takashi Mori <kanetugu2015@gmail.com>
2024-07-01 08:46:15 +0900

    fix #27

--- drone/Assets/Plugin/src/Unity/AR/Assets/VirtualPlayerSignalController.cs
+++ drone/Assets/Plugin/src/Unity/AR/Assets/VirtualPlayerSignalController.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Hakoniwa.AR.Core;
+using Hakoniwa.PluggableAsset;
+using Hakoniwa.PluggableAsset.Assets.Robot.Parts;
+using Hakoniwa.PluggableAsset.Communication.Connector;
+using Hakoniwa.PluggableAsset.Communication.Pdu;
 using UnityEngine;
 
 namespace Hakoniwa.AR.Assets
 {
-    public class VirtualSignalController : MonoBehaviour, IHakoPlayerState
+    public class VirtualSignalController : MonoBehaviour, IHakoPlayerState, IRobotPartsSensor, IRobotPartsConfig
     {
         public Renderer signal_red;
         public Renderer signal_yellow;
@@ -146,6 +151,95 @@ namespace Hakoniwa.AR.Assets
             }
             //Debug.Log("sate: " + state + " signal_sate:" + signal_state);
             return state;
+        }
+
+        public bool isAttachedSpecificController()
+        {
+            return false;
+        }
+
+        private uint GetPduSignalState()
+        {
+            return (uint)this.signal_state;
+        }
+        public void UpdateSensorValues()
+        {
+            uint pdu_data = GetPduSignalState();
+            Debug.Log("Write before: " + pdu_data);
+            this.pdu_writer.GetWriteOps().SetData("data", pdu_data);
+            Debug.Log("Write after: " + pdu_data);
+        }
+
+        private GameObject root;
+        private string root_name;
+        private PduIoConnector pdu_io;
+        private IPduWriter pdu_writer;
+        public string topic_type = "std_msgs/UInt32";
+        public string topic_name = "signal_data";
+
+
+        public void Initialize(object obj)
+        {
+            GameObject tmp = null;
+            try
+            {
+                tmp = obj as GameObject;
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.LogError("Initialize error: " + e.Message);
+                return;
+            }
+
+            if (this.root == null)
+            {
+                this.root = tmp;
+                this.root_name = string.Copy(this.root.transform.name);
+                this.pdu_io = PduIoConnector.Get(root_name);
+                if (this.pdu_io == null)
+                {
+                    throw new ArgumentException("can not found pdu_io:" + root_name);
+                }
+                var pdu_writer_name = root_name + "_" + this.topic_name + "Pdu";
+                this.pdu_writer = this.pdu_io.GetWriter(pdu_writer_name);
+                if (this.pdu_writer == null)
+                {
+                    throw new ArgumentException("can not found pdu_reader:" + pdu_writer_name);
+                }
+                Debug.Log("Pdu Writer is loaded:" + pdu_writer_name);
+            }
+        }
+        public RosTopicMessageConfig[] getRosConfig()
+        {
+            RosTopicMessageConfig[] cfg = new RosTopicMessageConfig[1];
+            cfg[0] = new RosTopicMessageConfig();
+            cfg[0].topic_message_name = this.topic_name;
+            cfg[0].topic_type_name = this.topic_type;
+            cfg[0].sub = false;
+            cfg[0].pub_option = new RostopicPublisherOption();
+            cfg[0].pub_option.cycle_scale = this.update_cycle;
+            cfg[0].pub_option.latch = false;
+            cfg[0].pub_option.queue_size = 1;
+            return cfg;
+        }
+
+        public IoMethod io_method = IoMethod.SHM;
+        public CommMethod comm_method = CommMethod.DIRECT;
+        public int update_cycle = 1;
+        public RoboPartsConfigData[] GetRoboPartsConfig()
+        {
+            RoboPartsConfigData[] configs = new RoboPartsConfigData[1];
+            configs[0] = new RoboPartsConfigData();
+            configs[0].io_dir = IoDir.WRITE;
+            configs[0].io_method = this.io_method;
+            configs[0].value.org_name = this.topic_name;
+            configs[0].value.type = this.topic_type;
+            configs[0].value.class_name = ConstantValues.pdu_writer_class;
+            configs[0].value.conv_class_name = ConstantValues.conv_pdu_writer_class;
+            configs[0].value.pdu_size = ConstantValues.PduMetaDataSize + 2 * sizeof(uint);
+            configs[0].value.write_cycle = this.update_cycle;
+            configs[0].value.method_type = this.comm_method.ToString();
+            return configs;
         }
     }
 }
