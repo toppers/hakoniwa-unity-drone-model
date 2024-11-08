bc1929bf67e0b56b8c0ccb10499558fe59515b28
Takashi Mori <kanetugu2015@gmail.com>
2024-11-08 12:22:02 +0900

    add installer for drones

---
+++ install_drones.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp drone/drone_config.json plugin/plugin-srcs/
+cp drone/hakoniwa_path.json plugin/plugin-srcs/
+cp -rp drone/Assets/* plugin/plugin-srcs/Assets/
+cp drone/Packages/* plugin/plugin-srcs/Packages/
+
